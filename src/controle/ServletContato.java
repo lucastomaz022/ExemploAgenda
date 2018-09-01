10688c9a1770b049cbc016bf957a95641c7e7a12
Anderson Menezes <anderson.menezes@rj.senac.br>
2018-08-31 21:35:56 -0300

    Commit inicial

---
+++ src/controle/ServletContato.java
@@ -0,0 +1,44 @@
+package controle;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * A anotação @WebServlet substitui o mapeamento do servlet
+ * realizado no arquivo web.xml (isso para a versão 3.0 ou
+ * superior do servlet. Entre parênteses passamos o url-pattern
+ * para o servlet.
+ * 
+ */
+@WebServlet("/novoContato")
+public class ServletContato extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest requisicao, 
+			HttpServletResponse resposta) throws ServletException, IOException {
+		
+		String nome = requisicao.getParameter("nomeContato");
+		String email = requisicao.getParameter("emailContato");
+		String telefone = requisicao.getParameter("telefoneContato");
+		String endereco = requisicao.getParameter("enderecoContato");
+		
+		// Processamento necessário para persistência
+		
+		resposta.sendRedirect("confirmacao.html");
+		
+	}
+
+}
+
+
+
+
+
+
+
+
